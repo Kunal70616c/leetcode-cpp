1de6f91c4c5f80db9a5dbcced5afe96c9547a5d3
Kunal Pal <kunal.pal1010@gmail.com>
2025-08-30 14:47:40 +0530

    Problem 145 Solved ✅

---
+++ problem_145/Binary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,32 @@
+// 145. Binary Tree Postorder Traversal
+//Given the root of a binary tree, return the postorder traversal of its nodes' values.
+//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> res;
+    void postorder(TreeNode* root){
+        if(root == nullptr){
+            return;
+        }
+        postorder(root->left);
+        postorder(root->right);
+        res.push_back(root->val);
+       
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        postorder(root);
+        return res;
+    }
+};
