f81f94d0fe3d50b3ba2275887b0c0175d02dc9e6
Kunal Pal <kunal.pal1010@gmail.com>
2025-08-31 22:43:34 +0530

    Problem 101 Solved ✅

---
+++ problem_101/Symmetric_Tree.cpp
@@ -0,0 +1,32 @@
+// 101. Symmetric Tree 
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q){
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
+            return false;
+        } else if (p->val != q->val) {
+            return false;
+        }
+        return ((isSameTree(p->left, q->right)) &&
+                (isSameTree(p->right, q->left)));
+    }
+    bool isSymmetric(TreeNode* root) {
+        return (isSameTree(root->left, root->right));
+    }
+};
